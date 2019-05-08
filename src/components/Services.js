2a7bf7fab90a2c93a2dfb7cf02e756a5153a4b64
lattrellsapon <lattrellsapon@gmail.com>
2019-05-08 21:19:38 +1200

    Removed priving and changed the heading for Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,21 +7,18 @@ export class Services extends Component {
   render() {
     return (
       <div className="container" id="services">
-        <h1>Services</h1>
+        <h1>What I can do</h1>
         <div className="three-column-grid">
             <div className="services-content">
                 <h3>One Page Website</h3>
-                <p>$50.00</p>
                 <img src={OnePage} alt=""/>
             </div>
             <div className="services-content">
                 <h3>Multipage Website</h3>
-                <p>$20.00 per additional page</p>
                 <img src={MultiplePages} alt=""/>
             </div>
             <div className="services-content">
                 <h3>Responsive Website</h3>
-                <p>All websites are responsive</p>
                 <img src={Responsive} alt=""/>
             </div>
         </div>
