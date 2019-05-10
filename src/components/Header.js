b229d9a0770896894fe4a73356883374415147fa
lattrellsapon <lattrellsapon@gmail.com>
2019-05-11 10:38:38 +1200

    Fixed card in the header -  added flex

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ export class Header extends Component {
   render() {
     return (
       <div id="header">
-        <div className="header-content">
-          <h1>Treezy Web Design</h1>
-          <p>Developing your ideas into reality.</p>
+        <div className="flex-container">
+          <div className="header-content">
+            <h1>Treezy Web Design</h1>
+            <p>Developing your ideas into reality.</p>
+          </div>
         </div>
       </div>
     )
