05d4c51c94266a686c45eb827e5183c491d6458c
lattrellsapon <lattrellsapon@gmail.com>
2019-05-11 02:03:48 +1200

    Added new bg in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ export class Header extends Component {
   render() {
     return (
       <div id="header">
-        <h1>Treezy Web Design</h1>
-        <p>Developing your ideas into reality.</p>
+        <div className="header-content">
+          <h1>Treezy Web Design</h1>
+          <p>Developing your ideas into reality.</p>
+        </div>
       </div>
     )
   }
